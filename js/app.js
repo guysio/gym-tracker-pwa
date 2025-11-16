a7406007d4a93e33cfd1e5c173194b976b1cc847
guysio <guyluxury6@gmail.com>
2025-11-16 16:08:04 +0000

    update link

--- js/app.js
+++ js/app.js
@@ -210,7 +210,7 @@ function closeTab() {
 
 // Sự kiện nút Check for Updates
 tab.querySelector('#update-sw').addEventListener('click', () => {
-    window.location.href = '/app/Views/update-progress.html';
+    window.location.href = './app/Views/update-progress.html';
 });
 actionBtn.addEventListener('click', () => openTab());
 tab.querySelector('#close-tab-SYS').addEventListener('click', () => closeTab());

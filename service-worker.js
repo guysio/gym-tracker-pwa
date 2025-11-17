700705924d865f27108de5724af924e97d97c97c
guysio <guyluxury6@gmail.com>
2025-11-17 10:09:21 +0000

    edit short name

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-gym-tracker-cache-v1';
+const CACHE_NAME = 'pwa-gym-tracker-cache-v2';
 const urlsToCache = [
     './app/Views/homepage.html',
     './app/Views/update-progress.html',
